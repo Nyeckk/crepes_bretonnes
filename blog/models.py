566ac617f4b6a6c34100b69cec44bd8d9609d1b2
Nyeckk <eteillearning@gmail.com>
2019-10-07 21:17:16 +0200

    Ajout des modeles Articles et Categorie

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Article(models.Model):
+    """
+    Classe representant un article.
+    """
+
+    titre = models.CharField(max_length=50)
+    auteur = models.CharField(max_length=100)
+    contenu = models.TextField(null=True)
+    date_parution = models.DateTimeField(default=timezone.now, verbose_name='date de parution')
+
+    categorie = models.ForeignKey('Categorie', on_delete=models.CASCADE)
+
+    class meta:
+        verbose_name = 'article'
+        ordering = ['date_parution']
+
+    def __str__(self):
+        return self.titre
+
+
+class Categorie(models.Model):
+    """
+    Classe represantant les differentes categories
+    """
+
+    nom = models.CharField(max_length=30)
+    
+    class meta():
+        verbose_name = 'categorie'
+        ordering = ['nom']
+    
+    def __str__(self):
+        return self.nom
--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ def home(request):
     """A function that return an http response of ourselve"""
     return HttpResponse("""
             <h1>Bienvenue sur mon blog !</h1>
-        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p
+            <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
         """)
 
 def view_article(request, id_article):
